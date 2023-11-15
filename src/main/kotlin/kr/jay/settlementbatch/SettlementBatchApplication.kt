a019e408d9eea34c53706e427f9187a024cc7136
jaypark <pjhyun1990@gmail.com>
2023-11-15 17:21:37 +0900

    init project

---
+++ src/main/kotlin/kr/jay/settlementbatch/SettlementBatchApplication.kt
@@ -0,0 +1,11 @@
+package kr.jay.settlementbatch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SettlementBatchApplication
+
+fun main(args: Array<String>) {
+    runApplication<SettlementBatchApplication>(*args)
+}
